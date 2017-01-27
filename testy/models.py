5f36aa5fb5db397517c31ea2040d1bdb183bad8b
Tomasz <tete10@wp.pl>
2017-01-27 10:31:44 +0100

    add Task model : table fields

--- testy/models.py
+++ testy/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+
+    title = models.CharField(max_length=200)
+    question = models.TextField()
+    answer1 = models.TextField()
+    answer2 = models.TextField()
+    answer3 = models.TextField()
+    answer4 = models.TextField()
+    corect = models.IntegerField()
