6bead44a0a5e6e560d6d2eacfe714700a42c8306
tthanquol <tete10@wp.pl>
2017-01-27 22:42:56 +0100

    add question list view

--- testy/views.py
+++ testy/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from testy.models import Question
 # Create your views here.
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def question_list(request):
+    questions = Question.objects.all()
+    return render(request, 'testy/question_list.html',{'questions' :questions})
