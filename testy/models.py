4f6a6bb6f8b043310196345bb71fb92de78dc805
Tomasz <tete10@wp.pl>
2017-01-27 12:56:52 +0100

    add Task model : table fields

--- testy/models.py
+++ testy/models.py
@@ -5,9 +5,12 @@ from django.db import models
 class Task(models.Model):
 
     title = models.CharField(max_length=200)
-    question = models.TextField()
+    questtext  = models.TextField()
     answer1 = models.TextField()
     answer2 = models.TextField()
     answer3 = models.TextField()
     answer4 = models.TextField()
     corect = models.IntegerField()
+
+    def __unicode__ (self):
+        return self.questtext
